deabe179257771850322ef8927f99305a8829130
Max Drexler <mdrexler@zazu.ssec.wisc.edu>
2023-01-19 21:40:34 +0000

    fixed bug where server connections wouldn't close

--- test/conftest.py
+++ test/conftest.py
@@ -1,14 +1,171 @@
 import pytest
-from amqp_server import TestAmqpServer
+import queue
+import pika
+import pika.channel
+import threading
 
-#TEST_SERVER = TestAmqpServer()
+AMQP_ROUTING_KEY = "*.*.*"
+AMQP_TEST_EXCHANGE = 'easymq_test'
 
-#@pytest.fixture(scope="session")
-#def get_server():
-#    return TEST_SERVER
+class AmqpClient:
+    
+    QUEUE_NAME = 'emq_standard'
+    
+    def __init__(self, routing_key, exchange_name, exchange_type='topic', connect_url = 'localhost', username='guest', password='guest') -> None:
+        self._routing_key = routing_key
+        self._exchange_name = exchange_name
+        self._exchange_type = exchange_type
+        
+        self.should_reconnect = False
+        self.was_consuming = False
+        
+        self._connection: pika.SelectConnection = None
+        self._channel: pika.channel.Channel = None
+        self._closing = False
+        self._url = connect_url
+        self._consuming = False
+        self._prefetch_count = 1
+        
+        self._recv_msgs = queue.Queue()
+        self._auth = pika.PlainCredentials(username, password, erase_on_connect=True)
+        
+    def connect(self):
+        """Connect to RabbitMQ
+        """
+        
+        return pika.SelectConnection(
+            parameters=pika.ConnectionParameters(host=self._url, credentials=self._auth),
+            on_open_callback=self.on_connection_open,
+            on_open_error_callback=self.on_connection_open_error,
+            on_close_callback=self.on_connection_closed
+        )
 
-#@pytest.fixture(autouse=True, scope="session")
-#def start_server():
-#    TEST_SERVER.start()
-#    yield
-#    TEST_SERVER.terminate()
+    def close_connection(self):
+        self._consuming = False
+        if self._connection is None:
+            return
+        if not self._connection.is_closing and self._connection.is_closed:
+            self._connection.close()
+            
+    def on_connection_open(self, _unused_connection):
+        print('connection opened')
+        self._connection.channel(on_open_callback=self.on_channel_open)
+    
+    def on_connection_closed(self, _unused_connection, reason):
+        print('connection closed')
+        self._channel = None
+        if self._closing:
+            self._connection.ioloop.stop()
+        else:
+            self.reconnect()
+    
+    def on_channel_open(self, channel):
+        self._channel = channel
+        self._channel.add_on_close_callback(self.on_channel_closed)    
+        self._channel.exchange_declare(
+            exchange=self._exchange_name,
+            exchange_type=self._exchange_type,
+            callback=self.on_exchange_declareok
+        )
+    
+    def on_channel_closed(self, channel, reason):
+        self.close_connection()
+    
+    def reconnect(self):
+        self.should_reconnect = True
+        self.stop()
+    
+    def on_exchange_declareok(self):
+        """setup queue
+        """
+        self._channel.queue_declare(queue=self.QUEUE_NAME, callback=self.on_queue_declareok)
+        
+    def on_queue_declareok(self, _unused_frame):
+        self._channel.queue_bind(
+            self.QUEUE_NAME,
+            self._exchange_name,
+            routing_key=self._routing_key,
+            callback=self.on_bindok
+        )
+        
+    def on_bindok(self, _unused_frame):
+        self._channel.basic_qos(
+            prefetch_count=self._prefetch_count, callback=self.on_basic_qos_ok
+        )
+        
+    def on_basic_qos_ok(self, _unused_frame):
+        self._channel.add_on_cancel_callback(self.on_consumer_cancelled)
+        self._consumer_tag = self._channel.basic_consume(
+            self.QUEUE_NAME, self.on_message
+        )
+        self.was_consuming = True
+        self._consuming = True
+        
+    def on_consumer_cancelled(self, method_frame):
+        if self._channel:
+            self._channel.close()
+    
+    def on_message(self, _unused_channel, basic_deliver, properties, body):
+        self._recv_msgs.put(body)
+        self._channel.basic_ack(basic_deliver.delivery_tag)
+    
+    def stop_consuming(self):
+        if self._channel:
+            self._channel.basic_cancel(self._consumer_tag, self.on_cancelok)
+
+    def on_cancelok(self, _unused_frame):
+        self._consuming = False
+        self.close_channel()
+        
+    def close_channel(self):
+        self._channel.close()
+        
+    def on_connection_open_error(self):
+        print('error while connecting')
+        self.reconnect()
+    
+    def get_message(self, block=True):
+        try:
+            return self._recv_msgs.get(block=block)
+        except queue.Empty:
+            return None
+    
+    def stop(self):
+        if not self._closing:
+            self._closing = True
+            if not self._connection:
+                return
+            if self._consuming:
+                self.stop_consuming()
+                self._connection.ioloop.start()
+            else:
+                self._connection.ioloop.start()
+
+
+@pytest.fixture(autouse=True, scope="session")
+def durable_consumer():
+    client = AmqpClient(AMQP_ROUTING_KEY, AMQP_TEST_EXCHANGE)
+    cl_thread = threading.Thread(target=client.connect)
+    cl_thread.start()
+    yield client
+    client.stop()
+    cl_thread.join()
+
+@pytest.fixture
+def consumer():
+    client = AmqpClient(AMQP_ROUTING_KEY, AMQP_TEST_EXCHANGE)
+    cl_thread = threading.Thread(target=client.connect)
+    cl_thread.start()
+    yield client
+    client.stop()
+    cl_thread.join()
+    
+    
+@pytest.fixture
+def remote_consumer(server):
+    client = AmqpClient(AMQP_ROUTING_KEY, AMQP_TEST_EXCHANGE, connect_url=server.param)
+    cl_thread = threading.Thread(target=client.connect)
+    cl_thread.start()
+    yield client
+    client.stop()
+    cl_thread.join()
--- test/send_test.py
+++ test/send_test.py
@@ -1,20 +1,27 @@
 from easymq import MQClient
 import pytest
 from pika.exceptions import AuthenticationError
+from conftest import AMQP_ROUTING_KEY, AMQP_TEST_EXCHANGE
 
-def test_creation():
-    pass
+TEST_MSG = (AMQP_ROUTING_KEY.replace('*','test'), {'received': 'received'})
 
 def test_false_server():
     with pytest.raises(ConnectionError):
-        client = MQClient('model', 'this.server.does.not.exist')
+        client = MQClient().connect('this.server.does.not.exist')
         
 def test_no_credentials():
     with pytest.raises(AuthenticationError):
-        client = MQClient('model', 'dcprod-dev.ssec.wisc.edu')
+        client = MQClient().connect('dcprod-dev.ssec.wisc.edu')
         
 def test_context_manager():
-    with MQClient('model', 'dcprod-dev.ssec.wisc.edu') as client:
-        pass
-    assert client.connections == []
-        
+    with MQClient().connect('localhost') as client:
+        assert client.open_connections == ['localhost']
+    assert client.open_connections == []
+
+@pytest.mark.skip
+def test_local_msg_send(durable_consumer):
+    client = MQClient().connect('localhost')
+    client.exchange = AMQP_TEST_EXCHANGE
+    client.send(TEST_MSG)
+    
+    assert durable_consumer.get_message() == TEST_MSG[1]
